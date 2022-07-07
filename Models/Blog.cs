013d174c4ae270a55ae64488036831ee9cf8c07c
airoble-1 <airoble2019@gmail.com>
2022-07-07 00:01:43 -0400

    Add blog model

---
+++ Models/Blog.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBlogProject.Models
+{
+    public class Blog
+    { 
+        public int Id { get; set; } 
+        public string AuthorId { get; set; }
+       
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be atleast {2} and at most {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be atleast {2} and at most {1} characters", MinimumLength = 2)]
+        public string Description { get; set; }
+        
+        [DataType(DataType.Date)] // treat as just as Date on the GUI not Data-time
+        [Display(Name = "Created Date")] // uses this string for the form field label instead of prop name
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)] // treat as just as Date on the GUI not Data-time
+        [Display(Name = "Updated Date")] // uses this string for the form field label instead of prop name
+        public DateTime? Updated { get; set; } // nullable date-time
+        
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }  // file byte stream
+        [Display(Name = "Blog Type")]
+        public string ContentType { get; set; } // image type
+        [NotMapped] // will not create an field in table
+        public IFormFile Image { get; set; } // represents the file object will derive details from to hold in other props above
+
+    }
+}
