f2422dbf142f8bd40bdb46647e1780a64cde495e
airoble-1 <airoble2019@gmail.com>
2022-07-07 22:35:54 -0400

    Add tag model

---
+++ Models/Tag.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBlogProject.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be atleast {2} and no more than {1} character long", MinimumLength = 2)]
+        public string Text { get; set; }
+
+        // navigation properties
+        public Post Post { get; set; }  
+        public virtual IdentityUser Author { get; set; }        
+
+
+    }
+}
