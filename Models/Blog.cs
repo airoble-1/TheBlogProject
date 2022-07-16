b1f7150b1f1a0514a365a8a04935a855130fe239
airoble-1 <airoble2019@gmail.com>
2022-07-16 15:32:21 -0400

    Clean up post create action/view

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -14,6 +14,7 @@ namespace TheBlogProject.Models
        
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be atleast {2} and at most {1} characters", MinimumLength = 2)]
+        [Display(Name = "Blog Name")]
         public string Name { get; set; }
         [Required]
         [StringLength(500, ErrorMessage = "The {0} must be atleast {2} and at most {1} characters", MinimumLength = 2)]
